966cfe14d07c5f940c0b31b94ae56125ba7e171f
Mikhail Biriukov <12212005b@gmail.com>
2024-06-09 12:03:44 +0300

    Добавьте файлы проекта.

---
+++ weather/DB_manipu;ations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weather
+{
+    internal class DB_manipu_ations
+    {
+        private static string conn_string = "Data Source = \"weather.db\";";
+
+        private static string init_query =
+            $"CREATE TABLE IF NOT EXISTS WEATHER_COND (" +
+            $"ID     INTEGER PRIMARY KEY AUTOINCREMENT," +
+            $"PRECIPITATION INTEGER," +
+            $"TEMPRETURE INTEGER," +
+            $" DATA TEXT);";
+
+
+        private static string init_data =
+            $"INSERT INTO WEATHER_COND (" +
+            $"PRECIPITATION, TEMPRETURE, DATE) " +
+            $"VALUES(" +
+            $" 50, 25, date('2021-11-29') );";
+
+        private static string select_all_data = "SELECT * FROM WEATHER_COND;";
+
+        public static void Prepare()
+        {
+            SQLiteConnection connection = new SQLiteConnection(conn_string);
+            connection.Open();
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = init_query;
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+        public static void InitData()
+        {
+            AddData(init_data);
+        }
+
+
+
+        public static void AddData(string query)
+        {
+            SQLiteConnection connection = new SQLiteConnection(conn_string);
+            connection.Open();
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+
+        public static string GetAllData(string separator = "\t|\t")
+        {
+            return GetData(select_all_data, separator);
+        }
+
+
+
+        public static string GetData(string query, string separator = "\t|\t")
+        {
+            string result = "";
+            SQLiteConnection connection = new SQLiteConnection(conn_string);
+            connection.Open();
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = query;
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    if (i == reader.FieldCount - 1)
+                    {
+                        result += reader.GetValue(i);
+                    }
+                    else
+                    {
+                        result += reader.GetValue(i) + separator;
+                    }
+                }
+                result += "\n";
+            }
+            connection.Close();
+            return result;
+        }
+    }
+}
---
+++ weather/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Security.Claims;
+using System.Xml.Linq;
+
+namespace weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int precipitation, temprature;
+            try
+            {
+                int.TryParse(args[0], out precipitation);
+                int.TryParse(args[1], out temprature);
+
+            }
+            catch
+            {
+                Console.WriteLine("осадки: ");
+                int.TryParse(Console.ReadLine(), out precipitation);
+                Console.WriteLine("температура: ");
+                int.TryParse(Console.ReadLine(), out temprature);
+            }
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string query =
+                $"INSERT INTO WEATHER_COND(" +
+            $"PRECIPITATION,  TEMPRETURE, DATE) " +
+            $"VALUES(" +
+            $"{precipitation}, {temprature}, date('{dateTime.ToString("yyyy-MM-dd")}') );";
+
+            string output = "";
+
+            Console.WriteLine($"Информация в базу данных: осадки: {precipitation}, температура:{temprature}, дата:{dateTime.ToString("yyyy-MM-dd")} ");
+       
+            DB_manipu_ations.Prepare();
+            DB_manipu_ations.AddData(query);
+
+            output = DB_manipu_ations.GetAllData(";");
+
+
+            Console.WriteLine("Вся сводка по осадкам");
+
+            var arr = output.Split(';');
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            /*DateTime date1 = new DateTime(); // год - месяц - день
+            Console.WriteLine(date1);*/
+
+            /*DateTime dateTime = DateTime.UtcNow.Date;
+            Console.WriteLine(dateTime.ToString("yyyy-MM-dd"));
+            Console.WriteLine(DateTime.Now);*/
+            /*  var alarm = DateTime.Now.Ticks;
+              Console.WriteLine(alarm.ToString())*/
+
+        }
+    }
+}
